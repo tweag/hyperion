6ac9b0d96db97f30f75153d679c593df78662e17
Mathieu Boespflug <m@tweag.io>
2016-12-31 14:20:36 +0100

    Implement namesOf. This depends on lens.

---
+++ src/Hyperion/Analysis.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Hyperion.Analysis
+  ( namesOf
+  ) where
+
+import Control.Lens (Contravariant(..), Fold, folded)
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Data.Monoid ((<>))
+import Data.Traversable (for)
+import Hyperion.Benchmark
+import Hyperion.Internal
+
+data Component = BenchC Text | GroupC Text | SeriesC Text
+
+qualName :: [Component] -> Text
+qualName = go ""
+  where
+    go index [BenchC txt] = txt <> index
+    go index (GroupC txt : comps) = txt <> index <> "/" <> go "" comps
+    go index (SeriesC txt : comps) = go (index <> ":" <> txt) comps
+    go _ _ = error "qualName: Impossible"
+
+namesOf :: Fold Benchmark Text
+namesOf = go []
+  where
+    go :: [Component] -> Fold Benchmark Text
+    go comps f (Bench name _) = coerce $ f (qualName (comps <> [BenchC name]))
+    go comps f (Group name bks) = coerce $ (folded.go (comps <> [GroupC name])) f bks
+    go comps f (Bracket _ _ g) = go comps f (g Empty)
+    go comps f (Series xs g) =
+      coerce $ for xs $ \x ->
+        go (comps <> [SeriesC (Text.pack (show x))]) f (g Empty)
+
+    coerce :: (Contravariant f, Applicative f) => f a -> f b
+    coerce = contramap (const ()) . fmap (const ())
