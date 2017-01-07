467927e8bdef6d4b84bcd205ba3eb7c1980794bc
Mathieu Boespflug <m@tweag.io>
2017-01-07 11:51:08 +0100

    Initial spec for Hyperion.Main.

---
+++ tests/Hyperion/MainSpec.hs
@@ -0,0 +1,18 @@
+module Hyperion.MainSpec where
+
+import Control.Lens ((^..))
+import Data.List (group, sort)
+import Hyperion.Analysis
+import Hyperion.BenchmarkSpec ()
+import Hyperion.Main
+import Test.Hspec
+import Test.QuickCheck ((==>), expectFailure, property)
+import Test.QuickCheck.Monadic (monadicIO, run)
+
+spec :: Spec
+spec = do
+    describe "defaultMain" $ do
+      it "checks for duplicate names" $ property $ \b ->
+        length (b^..namesOf) /= length (group (sort (b^..namesOf))) ==>
+        expectFailure $ monadicIO $ run $
+          defaultMainWith defaultConfig{configMonoidMode = return Run} [b]
