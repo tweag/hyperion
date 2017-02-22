9250750e476023d3465e2497ea3b031810f03fcc
Felix Raimundo <felix.raimundo@tweag.io>
2017-02-22 11:12:10 +0100

    Fix comments for realsies

--- src/Hyperion/PrintReport.hs
+++ src/Hyperion/PrintReport.hs
@@ -6,18 +6,17 @@
 module Hyperion.PrintReport (printReports) where
 
 import Control.Lens (view)
-import Control.Monad (forM_)
 import Data.Text (Text, unpack)
 import Data.HashMap.Strict (elems, HashMap)
 import Hyperion.Report
 import Text.PrettyPrint.ANSI.Leijen
 
-printReport :: Report -> IO ()
-printReport report = do
-    putDoc $ green (bold (text (unpack (view reportBenchName report)))) <> line
+formatReport :: Report -> Doc
+formatReport report =
+           green (bold (text (unpack (view reportBenchName report)))) <> line
            <> (indent 2 $ text ("Bench time: " ++ (show (view reportTimeInNanos report)) ++ "ns"))
            <> line
 
 printReports :: HashMap Text Report -> IO ()
 printReports report = do
-  forM_ (elems report) printReport
+  putDoc $ mconcat $ map formatReport (elems report)
